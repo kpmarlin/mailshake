29e42878620f4e80dde893343307f5bed3c8d9de
Kyle Marlin <kylemarlin@gmail.com>
2020-11-07 15:24:31 -0500

    Add some clarity around routing.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var clientRouter = require('./routes/client');
+var serverRouter = require('./routes/server');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', clientRouter);
+app.use('/api', serverRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
